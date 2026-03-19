76cac79885ef31ea84d8b8466d99768b6558863a
Jonghyeok Park <jonghyeok.park@unitblack.co.kr>
2026-03-20 00:06:12 +0900

    feat: add Service Metrics Monitor dashboard via Terraform
    APM 메트릭 데이터 뷰와 Spring WebFlux 서비스용 Kibana 대시보드를 추가한다.
    
    - Data view: metrics-apm.app.* (APM 앱 메트릭)
    - JVM Heap Memory (area, 서비스별 heap 사용량)
    - JVM CPU Utilization (line, kt-* 서비스)
    - JVM GC Duration (bar_stacked, kt-* 서비스)
    - R2DBC Connection Pool (line, kt-* 서비스)

--- otel-playground/infra/terraform/kibana.tf
+++ otel-playground/infra/terraform/kibana.tf
@@ -329,6 +329,320 @@ locals {
     for obj in [local.viz_log_volume, local.viz_log_level, local.viz_log_service, local.viz_log_trend, local.dashboard] :
     jsonencode(obj)
   ])
+
+  # --- Service Metrics Visualizations (APM metrics from OTel Agent) ---
+
+  viz_jvm_heap_memory = merge(local.lens_migration, {
+    id   = "viz-jvm-heap-memory"
+    type = "lens"
+    attributes = {
+      title             = "JVM Heap Memory"
+      visualizationType = "lnsXY"
+      state = {
+        visualization = {
+          legend              = { isVisible = true, position = "right" }
+          preferredSeriesType = "area"
+          layers = [{
+            layerId       = "layer1"
+            seriesType    = "area"
+            xAccessor     = "col-ts"
+            accessors     = ["col-mem"]
+            splitAccessor = "col-svc"
+            layerType     = "data"
+          }]
+        }
+        datasourceStates = {
+          formBased = {
+            layers = {
+              layer1 = {
+                columns = {
+                  col-ts = {
+                    label         = "@timestamp"
+                    dataType      = "date"
+                    operationType = "date_histogram"
+                    sourceField   = "@timestamp"
+                    isBucketed    = true
+                    params        = { interval = "auto" }
+                  }
+                  col-svc = {
+                    label         = "Service"
+                    dataType      = "string"
+                    operationType = "terms"
+                    sourceField   = "service.name"
+                    isBucketed    = true
+                    params = {
+                      size           = 10
+                      orderBy        = { type = "column", columnId = "col-mem" }
+                      orderDirection = "desc"
+                    }
+                  }
+                  col-mem = {
+                    label         = "Heap Used (bytes)"
+                    dataType      = "number"
+                    operationType = "average"
+                    sourceField   = "jvm.memory.used"
+                    isBucketed    = false
+                  }
+                }
+                columnOrder       = ["col-ts", "col-svc", "col-mem"]
+                incompleteColumns = {}
+              }
+            }
+          }
+        }
+        query   = { language = "kuery", query = "labels.jvm_memory_type: heap" }
+        filters = []
+      }
+      description = ""
+    }
+    references = [{
+      type = "index-pattern"
+      id   = "apm-metrics-data-view"
+      name = "indexpattern-datasource-layer-layer1"
+    }]
+  })
+
+  viz_jvm_cpu = merge(local.lens_migration, {
+    id   = "viz-jvm-cpu"
+    type = "lens"
+    attributes = {
+      title             = "JVM CPU Utilization"
+      visualizationType = "lnsXY"
+      state = {
+        visualization = {
+          legend              = { isVisible = true, position = "right" }
+          preferredSeriesType = "line"
+          layers = [{
+            layerId       = "layer1"
+            seriesType    = "line"
+            xAccessor     = "col-ts"
+            accessors     = ["col-cpu"]
+            splitAccessor = "col-svc"
+            layerType     = "data"
+          }]
+        }
+        datasourceStates = {
+          formBased = {
+            layers = {
+              layer1 = {
+                columns = {
+                  col-ts = {
+                    label         = "@timestamp"
+                    dataType      = "date"
+                    operationType = "date_histogram"
+                    sourceField   = "@timestamp"
+                    isBucketed    = true
+                    params        = { interval = "auto" }
+                  }
+                  col-svc = {
+                    label         = "Service"
+                    dataType      = "string"
+                    operationType = "terms"
+                    sourceField   = "service.name"
+                    isBucketed    = true
+                    params = {
+                      size           = 10
+                      orderBy        = { type = "column", columnId = "col-cpu" }
+                      orderDirection = "desc"
+                    }
+                  }
+                  col-cpu = {
+                    label         = "CPU Utilization"
+                    dataType      = "number"
+                    operationType = "average"
+                    sourceField   = "jvm.cpu.recent_utilization"
+                    isBucketed    = false
+                  }
+                }
+                columnOrder       = ["col-ts", "col-svc", "col-cpu"]
+                incompleteColumns = {}
+              }
+            }
+          }
+        }
+        query   = { language = "kuery", query = "service.name: kt-*" }
+        filters = []
+      }
+      description = ""
+    }
+    references = [{
+      type = "index-pattern"
+      id   = "apm-metrics-data-view"
+      name = "indexpattern-datasource-layer-layer1"
+    }]
+  })
+
+  viz_jvm_gc = merge(local.lens_migration, {
+    id   = "viz-jvm-gc"
+    type = "lens"
+    attributes = {
+      title             = "JVM GC Duration"
+      visualizationType = "lnsXY"
+      state = {
+        visualization = {
+          legend              = { isVisible = true, position = "right" }
+          preferredSeriesType = "bar_stacked"
+          layers = [{
+            layerId       = "layer1"
+            seriesType    = "bar_stacked"
+            xAccessor     = "col-ts"
+            accessors     = ["col-gc"]
+            splitAccessor = "col-svc"
+            layerType     = "data"
+          }]
+        }
+        datasourceStates = {
+          formBased = {
+            layers = {
+              layer1 = {
+                columns = {
+                  col-ts = {
+                    label         = "@timestamp"
+                    dataType      = "date"
+                    operationType = "date_histogram"
+                    sourceField   = "@timestamp"
+                    isBucketed    = true
+                    params        = { interval = "auto" }
+                  }
+                  col-svc = {
+                    label         = "Service"
+                    dataType      = "string"
+                    operationType = "terms"
+                    sourceField   = "service.name"
+                    isBucketed    = true
+                    params = {
+                      size           = 10
+                      orderBy        = { type = "column", columnId = "col-gc" }
+                      orderDirection = "desc"
+                    }
+                  }
+                  col-gc = {
+                    label         = "GC Duration"
+                    dataType      = "number"
+                    operationType = "average"
+                    sourceField   = "jvm.gc.duration"
+                    isBucketed    = false
+                  }
+                }
+                columnOrder       = ["col-ts", "col-svc", "col-gc"]
+                incompleteColumns = {}
+              }
+            }
+          }
+        }
+        query   = { language = "kuery", query = "service.name: kt-*" }
+        filters = []
+      }
+      description = ""
+    }
+    references = [{
+      type = "index-pattern"
+      id   = "apm-metrics-data-view"
+      name = "indexpattern-datasource-layer-layer1"
+    }]
+  })
+
+  viz_db_pool = merge(local.lens_migration, {
+    id   = "viz-db-pool"
+    type = "lens"
+    attributes = {
+      title             = "R2DBC Connection Pool"
+      visualizationType = "lnsXY"
+      state = {
+        visualization = {
+          legend              = { isVisible = true, position = "right" }
+          preferredSeriesType = "line"
+          layers = [{
+            layerId       = "layer1"
+            seriesType    = "line"
+            xAccessor     = "col-ts"
+            accessors     = ["col-conn"]
+            splitAccessor = "col-svc"
+            layerType     = "data"
+          }]
+        }
+        datasourceStates = {
+          formBased = {
+            layers = {
+              layer1 = {
+                columns = {
+                  col-ts = {
+                    label         = "@timestamp"
+                    dataType      = "date"
+                    operationType = "date_histogram"
+                    sourceField   = "@timestamp"
+                    isBucketed    = true
+                    params        = { interval = "auto" }
+                  }
+                  col-svc = {
+                    label         = "Service"
+                    dataType      = "string"
+                    operationType = "terms"
+                    sourceField   = "service.name"
+                    isBucketed    = true
+                    params = {
+                      size           = 10
+                      orderBy        = { type = "column", columnId = "col-conn" }
+                      orderDirection = "desc"
+                    }
+                  }
+                  col-conn = {
+                    label         = "Active Connections"
+                    dataType      = "number"
+                    operationType = "max"
+                    sourceField   = "db.client.connections"
+                    isBucketed    = false
+                  }
+                }
+                columnOrder       = ["col-ts", "col-svc", "col-conn"]
+                incompleteColumns = {}
+              }
+            }
+          }
+        }
+        query   = { language = "kuery", query = "service.name: kt-*" }
+        filters = []
+      }
+      description = ""
+    }
+    references = [{
+      type = "index-pattern"
+      id   = "apm-metrics-data-view"
+      name = "indexpattern-datasource-layer-layer1"
+    }]
+  })
+
+  metrics_dashboard = merge(local.dashboard_migration, {
+    id   = "service-metrics-dashboard"
+    type = "dashboard"
+    attributes = {
+      title       = "Service Metrics Monitor"
+      description = "JVM heap memory, CPU utilization, GC duration, and R2DBC connection pool for Spring WebFlux services"
+      panelsJSON  = jsonencode([
+        { panelIndex = "m1", gridData = { x = 0, y = 0, w = 24, h = 12, i = "m1" }, type = "lens", panelRefName = "panel_m1" },
+        { panelIndex = "m2", gridData = { x = 24, y = 0, w = 24, h = 12, i = "m2" }, type = "lens", panelRefName = "panel_m2" },
+        { panelIndex = "m3", gridData = { x = 0, y = 12, w = 24, h = 12, i = "m3" }, type = "lens", panelRefName = "panel_m3" },
+        { panelIndex = "m4", gridData = { x = 24, y = 12, w = 24, h = 12, i = "m4" }, type = "lens", panelRefName = "panel_m4" },
+      ])
+      timeRestore = true
+      timeTo      = "now"
+      timeFrom    = "now-1h"
+      kibanaSavedObjectMeta = {
+        searchSourceJSON = jsonencode({ query = { language = "kuery", query = "" }, filter = [] })
+      }
+    }
+    references = [
+      { name = "panel_m1", type = "lens", id = "viz-jvm-heap-memory" },
+      { name = "panel_m2", type = "lens", id = "viz-jvm-cpu" },
+      { name = "panel_m3", type = "lens", id = "viz-jvm-gc" },
+      { name = "panel_m4", type = "lens", id = "viz-db-pool" },
+    ]
+  })
+
+  metrics_saved_objects_ndjson = join("\n", [
+    for obj in [local.viz_jvm_heap_memory, local.viz_jvm_cpu, local.viz_jvm_gc, local.viz_db_pool, local.metrics_dashboard] :
+    jsonencode(obj)
+  ])
 }
 
 resource "elasticstack_kibana_import_saved_objects" "visualizations_and_dashboard" {
@@ -336,4 +650,23 @@ resource "elasticstack_kibana_import_saved_objects" "visualizations_and_dashboar
   file_contents = local.saved_objects_ndjson
 
   depends_on = [elasticstack_kibana_data_view.app_logs]
+}
+
+# --- APM Metrics Data View & Dashboard ---
+
+resource "elasticstack_kibana_data_view" "apm_metrics" {
+  data_view = {
+    id              = "apm-metrics-data-view"
+    name            = "metrics-apm.app.*"
+    title           = "metrics-apm.app.*"
+    time_field_name = "@timestamp"
+  }
+  override = true
+}
+
+resource "elasticstack_kibana_import_saved_objects" "metrics_dashboard" {
+  overwrite     = true
+  file_contents = local.metrics_saved_objects_ndjson
+
+  depends_on = [elasticstack_kibana_data_view.apm_metrics]
 }
